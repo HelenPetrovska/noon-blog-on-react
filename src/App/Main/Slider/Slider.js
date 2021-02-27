9a32b72410a7707df0aa3322a642fb2907216e78
Helen <paznachka@gmail.com>
2021-02-27 17:44:25 +0200

    add slider swiper

--- src/App/Main/Slider/Slider.js
+++ src/App/Main/Slider/Slider.js
@@ -1,18 +1,32 @@
+import SwiperCore, { Navigation, Pagination, Autoplay, A11y, EffectFade } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/swiper-bundle.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/effect-fade/effect-fade.min.css'
+import './slider.css'
+
+
+SwiperCore.use([Navigation, Pagination, Autoplay, A11y, EffectFade]);
 
 export default () => {
-  return (
-    <Swiper
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
-  );
-};
+    return (
+      <Swiper effect="fade"
+        spaceBetween={50}
+        slidesPerView={3}
+        pagination={{ clickable: true }}
+        navigation
+        autoplay>
+        {[
+            <div className="slide slide1"></div>,
+            <div className="slide slide2"></div>,
+            <div className="slide slide3"></div>
+         ].map((el) => {
+          return <SwiperSlide>{el}</SwiperSlide>;
+        })}
+      </Swiper>
+    );
+  };
+  
+
